1de0eae388eeca7618b27e646add2d715370b6e3
TeqbM <mukesh@teqbuddies.com>
2024-04-15 19:09:52 +0530

    Header footer Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Components/Pages/Home'
+import About from './Components/Pages/About'
+import Contact from './Components/Pages/Contact'
+import Notfound404 from './Components/Pages/Notfound'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
-  <Header />
-  <Home />
-  <Footer />
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<Notfound404 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function index() {
   return (
-    <>
-     <h1>Footer </h1>
-    </>
+    <footer className='py-3 bg-orange-50 border-t border-orange-200  fixed bottom-0 inset-x-0'>
+     <div className="container">
+      <div className="text-center text-orange-600">
+        Lorem ipsum dolor sit amet.
+      </div>
+     </div>
+    </footer>
   )
 }
