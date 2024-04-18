d6b4cf6b094d04c88e3aaf29a1afbac44acdcd67
TeqbM <mukesh@teqbuddies.com>
2024-04-18 14:36:12 +0530

    Swiper slider add

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,9 +1,10 @@
+import Swiper from '../Swiper'
 export default function Contact() {
 
   return (
     <section className="py-10">
       <div className="container">
-        
+          <Swiper />
 
       </div>
     </section>
