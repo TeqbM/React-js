fff1285e066254ff85492c5961109b47a76fba0a
TeqbM <mukesh@teqbuddies.com>
2024-04-18 11:29:08 +0530

    remove text counter in contact us page

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,28 +1,9 @@
-import React, { useState } from "react";
-
 export default function Contact() {
-  const [text, setText] = useState("");
 
-  const UpperCase =()=>{
-      setText(text.toUpperCase())
-  }
-  const LowerCase =()=>{
-    setText(text.toLowerCase())
-  }
-  const CapitaLize =()=>{
-    let cap = text.trim().split(" ")
-    
-    console.log(cap);
-
-  }
   return (
     <section className="py-10">
       <div className="container">
-        <textarea name="" id="" cols="30" value={text} rows="5" onChange={(e)=> setText(e.target.value)}></textarea>
-        <br />
-        <button className="btn" onClick={UpperCase}>UpprCase</button>
-        <button className="btn" onClick={CapitaLize}>CapitaLize</button>
-        <button className="btn" onClick={LowerCase}>LowerCase</button>
+        
 
       </div>
     </section>
