a4eac5f393d73ea5da4107a33e7d6323df2cf475
TeqbM <mukesh@teqbuddies.com>
2024-04-23 19:40:56 +0530

    dewsign changes

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,10 +1,12 @@
 import Userlist from "../Userlist";
 
 export default function Contact() {
+  let user = { name: "John" };
+  console.log(user);
   return (
     <section className='py-10'>
       <div className="container">
-        <Userlist />
+
       </div>
     </section>
   );
